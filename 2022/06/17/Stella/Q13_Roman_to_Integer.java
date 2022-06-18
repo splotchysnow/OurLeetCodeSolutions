aa4515958704a3845e6a91b2bf56c0701f27c5e8
yinshinLi <93513827+yinshinLi@users.noreply.github.com>
2022-06-18 16:36:52 +0800

    Update and rename Q13_Roman_to_Integer.cpp to Q13_Roman_to_Integer.java

---
+++ 2022/06/17/Stella/Q13_Roman_to_Integer.java
@@ -0,0 +1,28 @@
+import java.util.Arrays;
+class Solution {
+    public int romanToInt(String s) {
+        Map<Character,Integer> roman = new HashMap<>();
+        int result = 0;
+        
+        roman.put('I',1);
+        roman.put('V',5);
+        roman.put('X',10);
+        roman.put('L',50);
+        roman.put('C',100);  
+        roman.put('D',500);  
+        roman.put('M',1000);
+        
+        for(int i=0;i<s.length();i++){
+            if(i>0 && roman.get(s.charAt(i)) > roman.get(s.charAt(i-1)))
+            {
+                result += roman.get(s.charAt(i)) - 2*roman.get(s.charAt(i-1));
+            }
+    
+            else
+                result += roman.get(s.charAt(i));
+            }
+        return result;
+            
+    
+    }
+};
