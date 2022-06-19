6fc436dba72d932de31bd00a4d241f089d4d953c
yinshinLi <93513827+yinshinLi@users.noreply.github.com>
2022-06-20 04:01:58 +0800

    Q67

---
+++ 2022/06/19/Stella/Q67_Add_Binary.cpp
@@ -0,0 +1,32 @@
+//Given two binary strings a and b, return their sum as a binary string.
+
+class Solution {
+public:
+    string addBinary(string a, string b) {
+        string result;
+        int carry=0;
+        for(int i=0; i<max(a.length(),b.length()); i++){
+            while(a.length()>b.length()){
+                b = "0"+b;
+            }
+            while(a.length()<b.length()){
+                a = "0"+a;
+            }
+            if(carry==0 && (a[a.length()-1-i]!=(b[b.length()-1-i]))||carry==1 && (a[a.length()-1-i]==(b[b.length()-1-i]))){
+                result = "1" + result;
+            }else{
+                result = "0" + result;
+            }
+            if((carry==1&&a[a.length()-1-i]=='1')||(carry==1&&b[b.length()-1-i]=='1')||(a[a.length()-1-i]=='1'&&b[b.length()-1-i]=='1')){
+                carry=1;
+            }else{
+                carry=0;
+            }
+        }
+        if(carry==1){
+            result = "1"+result;
+        }
+        return result;
+    }
+};
+
