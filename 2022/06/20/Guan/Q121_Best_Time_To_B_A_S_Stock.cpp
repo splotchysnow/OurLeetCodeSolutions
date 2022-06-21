7c3a8d419c697d74375f8b468e24476c9d62cb11
splotchysnow <46405838+splotchysnow@users.noreply.github.com>
2022-06-20 17:31:25 -0700

    Update Q121_Best_Time_To_B_A_S_Stock.cpp
    Finished.... same as stella's

--- 2022/06/20/Guan/Q121_Best_Time_To_B_A_S_Stock.cpp
+++ 2022/06/20/Guan/Q121_Best_Time_To_B_A_S_Stock.cpp
@@ -3,7 +3,6 @@
 #include <map>
 #include <unordered_map>
 
-
 using namespace std;
 
 class Solution {
@@ -15,22 +14,17 @@ public:
         //Loop through every single elemnt in the list;
         for(int i = 0; i < prices.size()-1; i++){
             // cout << min;
+            //UPDATE MIN:
             //If the prices is smaller than min, store it.
             if(prices[i] < min){
                 min = prices[i];
             }
-            //if min is the same as ith position, loop next.
-            if(prices[i] == min){
+            //IF I IS SMALLER THAN MIN: GET IT:
+            if((prices[i+1] - min) > max){
+                //update max:
+                max = prices[i+1] - min;
                 //begin loop next couple elements.
-               for(int j = i+1; j < prices.size(); j++){
-                   int varj = prices[j];
-                   int vari = prices[i];
-                   int newMax = varj-vari;
-                   //if the j is bigger than i and bigger than max.
-                   if(varj > vari && newMax > max){
-                       max = varj-vari;
-                   }
-               }
+               
            }
         }
         return max;
