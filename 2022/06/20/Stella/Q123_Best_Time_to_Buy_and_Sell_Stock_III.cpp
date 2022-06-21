7612d49463ed17cd72bc4f68cc78149c19b73ca7
yinshinLi <93513827+yinshinLi@users.noreply.github.com>
2022-06-21 09:59:25 +0800

    Q123 processing 2

--- 2022/06/20/Stella/Q123_Best_Time_to_Buy_and_Sell_Stock_III.cpp
+++ 2022/06/20/Stella/Q123_Best_Time_to_Buy_and_Sell_Stock_III.cpp
@@ -12,6 +12,9 @@ class Solution {
 public:
     int maxProfit(vector<int>& prices) {
         int low = 10000;
+        int prev = 0;
+        int low1 = 0;
+        int low2 = 0;
         int increased = 0; //store profit increased in previous round, default 0
         int profit1 = 0;
         int profit2 = 0;
@@ -25,22 +28,37 @@ public:
                     profit1 += prices[i+1]-low;
                     cout<<"inc i:"<<i<<" ";
                     cout<<"p1:"<<profit1<<" ";
+                    low = 10000;
                 }else if(increased == 2){
                     profit2 += prices[i+1]-low;
                     cout<<"inc i:"<<i<<" ";
                     cout<<"p2:"<<profit2<<" ";
+                    low = 10000;
                 }else if(profit1>profit2 && (prices[i+1]-low)>profit2){
                     profit2 = prices[i+1]-low;
                     increased = 2;
+                    if(low2<low1 && prev == 1){
+                        profit1+=low1-low2;
+                        low1 = low2;
+                    }
+                    prev = 2;
+                    low2 = low;
                     cout<<"i:"<<i<<" ";
                     cout<<"p2:"<<profit2<<" ";
+                    low = 10000;
                 }else if((prices[i+1]-low)>profit1){
                     profit1 = prices[i+1]-low;
                     increased = 1;
+                    if(low2>low1 && prev == 2){
+                        profit2+=low2-low1;
+                        low2 = low1;
+                    }
+                    prev = 1;
+                    low1 = low;
                     cout<<"i:"<<i<<" ";
                     cout<<"p1:"<<profit1<<" ";
+                    low = 10000;
                 }
-                low = 10000;
             }else{
                 increased = 0;
             }
