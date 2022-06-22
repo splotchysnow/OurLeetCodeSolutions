519763340d2e8e1dcef965ca03f29332e87e00f8
yinshinLi <93513827+yinshinLi@users.noreply.github.com>
2022-06-22 14:24:54 +0800

    add_folder

---
+++ 2022/06/21/Guan/Q2260_Minimum_Consecutive_Cards_to_Pick_Up.cpp
@@ -0,0 +1,20 @@
+// You are given an integer array cards where cards[i] represents the value of the ith card. A pair of cards are matching if the cards have the same value.
+
+// Return the minimum number of consecutive cards you have to pick up to have a pair of matching cards among the picked cards. If it is impossible to have matching cards, return -1.
+
+class Solution {
+public:
+    int minimumCardPickup(vector<int>& cards) {
+        //store the last previous index of cards
+        unordered_map<int, int> cardIndex;   
+        int minLength = INT_MAX;
+        for(int i = 0; i<cards.size(); i++){
+            if(cardIndex.count(cards[i])){
+                minLength = min(minLength,i-cardIndex[cards[i]]+1);
+            }
+                cardIndex[cards[i]] = i;
+        }
+        if (minLength==INT_MAX)return -1;
+        return minLength;
+    }
+};
