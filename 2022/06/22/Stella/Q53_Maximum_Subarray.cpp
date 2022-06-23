597abd6d4753065da25bf930b2693373efd45886
yinshinLi <93513827+yinshinLi@users.noreply.github.com>
2022-06-23 14:01:57 +0800

    Q53

---
+++ 2022/06/22/Stella/Q53_Maximum_Subarray.cpp
@@ -0,0 +1,22 @@
+// Given an integer array nums, find the contiguous subarray (containing at least one number) which has the largest sum and return its sum.
+
+// A subarray is a contiguous part of an array.
+
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        int max = INT_MIN;
+        int maxInclude = 0;
+        for(int i = 0; i<nums.size(); i++){
+            maxInclude = maxInclude + nums[i];
+            if(maxInclude>max){
+                max = maxInclude;
+            }
+            if(maxInclude<0){
+                maxInclude = 0;
+            }
+        }
+        return max;
+        
+    }
+};
