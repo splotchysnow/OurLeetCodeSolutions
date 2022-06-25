ca1a2209da30a4fcd2ebca7e1eb690f460b13a1e
yinshinLi <93513827+yinshinLi@users.noreply.github.com>
2022-06-25 18:42:43 +0800

    q665 85%

---
+++ 2022/06/24/Q665_Non-decreasing_Array.cpp
@@ -0,0 +1,24 @@
+// Given an array nums with n integers, your task is to check if it could become non-decreasing by modifying at most one element.
+
+// We define an array is non-decreasing if nums[i] <= nums[i + 1] holds for every i (0-based) such that (0 <= i <= n - 2).
+
+class Solution {
+public:
+    bool checkPossibility(vector<int>& nums) {
+        int max = 0;
+        int last = 0;
+        int chance = 0;
+        for(int i = 1; i<nums.size();i++){
+            cout<<last<<chance<<" ";
+            if(chance==1 && (nums[i-1]>nums[i]||nums[i]<last)){
+                return false;
+            }else if(nums[i-1]>nums[i]){
+                chance++;
+                last = nums[i-1];
+            }else{
+                last = nums[i];
+            }
+        }
+        return true;
+    }
+};
