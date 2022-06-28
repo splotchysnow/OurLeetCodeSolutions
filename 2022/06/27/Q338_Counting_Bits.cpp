c8cc076b8cda61d28d3046a26b0a025dc760b56a
splotchysnow <46405838+splotchysnow@users.noreply.github.com>
2022-06-28 00:17:44 -0700

    Finished Q338
    Co-Authored-By: yinshinLi <93513827+yinshinLi@users.noreply.github.com>

--- 2022/06/27/Q338_Counting_Bits.cpp
+++ 2022/06/27/Q338_Counting_Bits.cpp
@@ -20,44 +20,71 @@ public:
         vector<int> ans;
         // cout << log2(5);
         // cout  << floor(5.5);
-        recursionBits(ans,n,n);
+        // recursionBits(ans,n,n,0);
+        
+        int lastGap = 0;
+        
+        for(int i = 0; i <= n; i++){
+            if(i == 0){
+                ans.push_back(0);
+            }
+            else if(i == 1){
+                ans.push_back(1);
+            }
+            else if(log2(i) == floor(log2(i))){
+                ans.push_back(1);
+                lastGap = i;
+            }
+            else{
+                // cout << lastGap;
+                ans.push_back(1+ans[i-lastGap]);
+            }
+        }
+        
+        
         return ans;
     }
     
-    vector<int> recursionBits(vector<int> &ans, int n, int count){
-        //This is ok:
-        if(count == 0){
-            //Find the log:
-            ans.push_back(log2(n-count));
-            count--;
-            return ans;
-        }
-        //This is first statement?
-        if(n-count == 0){
-            ans.push_back(0);
-            count--;
-            recursionBits(ans, n, count);
-        }
-        else if(n-count == 1){
-            ans.push_back(1);
-            count--;
-            recursionBits(ans, n, count);
-        }
-        else{
-            if(log2(n-count) == floor(log2(n-count))){
-                //If it has no decimal:
-                //Find the log:
-                ans.push_back(1);
-                count--;
-                recursionBits(ans,n, count);
-            }
-            else{
-                // Log2(n-count) + 1: gives you the Highest bit.
-                ans.push_back(log2(n-count));
-                count--;
-                recursionBits(ans,n, count);
-            }
-        }
-        return ans;
-    }
+    // vector<int> recursionBits(vector<int> &ans, int n, int count, int lastGap){
+    //     //This is first statement?
+    //     if(n-count == 0){
+    //         ans.push_back(0);
+    //         if(count == 0){
+    //             return ans;
+    //         }
+    //         count--;
+    //         recursionBits(ans, n, count,0);
+    //     }
+    //     else if(n-count == 1){
+    //         ans.push_back(1);
+    //         if(count == 0){
+    //             return ans;
+    //         }
+    //         count--;
+    //         recursionBits(ans, n, count,1);
+    //     }
+    //     else{
+    //         if(log2(n-count) == floor(log2(n-count))){
+    //             //If it has no decimal:
+    //             //Find the log:
+    //             ans.push_back(1);
+    //             lastGap = n-count;
+    //             if(count == 0){
+    //                 return ans;
+    //             }
+    //             count--;
+    //             recursionBits(ans,n, count,lastGap);
+    //         }
+    //         else{
+    //             // Log2(n-count) + 1: gives you the Highest bit.
+    //             ans.push_back(1+ans[(n-count)-lastGap]);
+    //             if(count == 0){
+    //                 return ans;
+    //             }
+    //             count--;
+    //             recursionBits(ans,n, count,lastGap);
+    //         }
+    //     }
+    //     return ans;
+    // }
 };
