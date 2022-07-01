0d12d1f7c5c4ffe1d3e6993e9ed61bf09c1c2784
yinshinLi <93513827+yinshinLi@users.noreply.github.com>
2022-07-01 13:53:40 +0800

    Create Q263_Ugly_Number.cpp
    Q263

---
+++ 2022/07/01/Stella/Q263_Ugly_Number.cpp
@@ -0,0 +1,17 @@
+// An ugly number is a positive integer whose prime factors are limited to 2, 3, and 5.
+
+// Given an integer n, return true if n is an ugly number.
+
+class Solution {
+public:
+    bool isUgly(int n) {
+        if (n<1) return false;
+        while(n>1){
+            if(n%2==0) n = n/2;
+            else if(n%3==0) n = n/3;
+            else if(n%5==0) n = n/5;
+            else return false;
+        }
+        return true;
+    }
+};
