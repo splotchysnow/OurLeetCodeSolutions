04d850807dc69eaa95ee707d0769f9f4d2622016
yinshinLi <93513827+yinshinLi@users.noreply.github.com>
2022-07-07 16:12:50 +0800

    Create Q5_Longest_Palindromic_Substring.cpp
    Q5
    
    Co-Authored-By: Guan Li <46405838+splotchysnow@users.noreply.github.com>

---
+++ 2022/07/07/Stella/Q5_Longest_Palindromic_Substring.cpp
@@ -0,0 +1,61 @@
+// Given a string s, return the longest palindromic substring in s.
+
+ 
+
+// Example 1:
+
+// Input: s = "babad"
+// Output: "bab"
+// Explanation: "aba" is also a valid answer.
+
+class Solution {
+public:
+    string longestPalindrome(string s) {
+        int start = 0;
+        int ms = 0;
+        int me = -1;
+        int end = 0;
+        bool cont = true;
+        for(int i = 0; i <= s.length()-1; i++){
+            start = i;
+            end = i;
+            while(cont){
+                if(start>=0 && end<=s.length()-1){
+                    //cout<<s[start]<<s[end]<<" ";
+                    if (s[start]!=s[end]){
+                        cont = false;
+                    }else{
+                        if((end-start)>=(me-ms)){
+                            ms = start;
+                            me = end;
+                        }
+                    }
+                }else{
+                    cont = false;
+                }
+                start--;
+                end++;
+            }
+            cont = true;
+            while(cont){
+                if(start>=0 && end<=s.length()-1){
+                    cout<<s[start]<<s[end-1]<<" ";
+                    if (s[start]!=s[end-1]){
+                        cont = false;
+                    }else{
+                        if((end-1-start)>=(me-ms)){
+                            ms = start;
+                            me = end-1;
+                        }
+                    }
+                }else{
+                    cont = false;
+                }
+                start--;
+                end++;
+            }
+            cont = true;
+        }
+        return s.substr(ms,me-ms+1);
+    }
+};
