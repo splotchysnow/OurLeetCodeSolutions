071c24f0a35100ff6a9918364d479798d094b5ee
splotchysnow <46405838+splotchysnow@users.noreply.github.com>
2022-07-07 23:08:06 -0700

    Create Q789_Escape_The_Ghosts.py
    finsihed, had online help

---
+++ 2022/07/07/Guan/Q789_Escape_The_Ghosts.py
@@ -0,0 +1,14 @@
+class Solution:
+    def escapeGhosts(self, ghosts: List[List[int]], target: List[int]) -> bool:
+        #The d abs distance of target.
+        d = abs(target[0]) + abs(target[1])
+        
+        #Get through all ghosts.
+        for ghost in ghosts:
+            #Distance is the difference in targetx from ghost minius distance in targety ghost.
+            distance = abs(target[0] - ghost[0]) + abs(target[1] - ghost[1])
+            
+            if distance <= d:
+                return False
+            
+        return True
