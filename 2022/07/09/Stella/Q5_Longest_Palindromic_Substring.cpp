d0331222b2a464514479bec30c495ce689be218c
yinshinLi <93513827+yinshinLi@users.noreply.github.com>
2022-07-09 13:44:29 +0800

    Update Q5_Longest_Palindromic_Substring.cpp
    Q5 solution
    
    Co-Authored-By: Guan Li <46405838+splotchysnow@users.noreply.github.com>

--- 2022/07/09/Stella/Q5_Longest_Palindromic_Substring.cpp
+++ 2022/07/09/Stella/Q5_Longest_Palindromic_Substring.cpp
@@ -1,1 +1,59 @@
 // Given a string s, return the longest palindromic substring in s.
+
+
+class Solution {
+public:
+    string longestPalindrome(string s) {
+        int start = 0;
+        int ms = 0;
+        int me = -1;
+        int end = 0;
+        bool cont = true;
+        for(int i = 0; i <= s.length()-1; i++){
+            start = i;
+            end = i;
+            while(cont){
+                if(start>=0 && end<=s.length()-1){
+                    //cout<<s[start]<<s[end]<<" ";
+                    if (s[start]!=s[end]){
+                        cont = false;
+                    }else{
+                        if((end-start)>=(me-ms)){
+                            ms = start;
+                            me = end;
+                        }
+                    }
+                }else{
+                    cont = false;
+                }
+                start--;
+                end++;
+            }
+            cont = true;
+        }
+        
+        for(int i = 0.5; i <= s.length()-1; i++){
+            start = i-0.5;
+            end = i+0.5;
+            while(cont){
+                if(start>=0 && end<=s.length()-1){
+                    //cout<<s[start]<<s[end]<<" ";
+                    if (s[start]!=s[end]){
+                        cont = false;
+                    }else{
+                        if((end-start)>=(me-ms)){
+                            ms = start;
+                            me = end;
+                        }
+                    }
+                }else{
+                    cont = false;
+                }
+                start--;
+                end++;
+            }
+            cont = true;
+        }
+        return s.substr(ms,me-ms+1);
+    }
+};
