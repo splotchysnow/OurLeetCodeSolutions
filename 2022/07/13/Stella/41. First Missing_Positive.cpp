e3106ebc2fb337340e65c41225fdba38da3c090a
yinshinLi <93513827+yinshinLi@users.noreply.github.com>
2022-07-13 17:17:05 +0800

    Create 41. First Missing_Positive.cpp
    Q41
    
    Co-Authored-By: Guan Li <46405838+splotchysnow@users.noreply.github.com>

---
+++ 2022/07/13/Stella/41. First Missing_Positive.cpp
@@ -0,0 +1,36 @@
+// Given an unsorted integer array nums, return the smallest missing positive integer.
+
+// You must implement an algorithm that runs in O(n) time and uses constant extra space.
+
+class Solution {
+public:
+    int firstMissingPositive(vector<int>& nums) {
+        //vector<int> sm; //smallest missing
+        
+        vector<int> set;
+        for(int i=1; i<=nums.size(); i++){
+            set.push_back(i);
+        }
+        
+        for(int j=0; j<nums.size(); j++){
+            for(int k=0; k<set.size(); k++){
+                if(set[k]==nums[j]){
+                    set.erase(set.begin()+k);
+                }
+            }
+        }
+        
+        for(int m=0; m<set.size(); m++){
+            cout<<set[m];
+        }
+    
+        
+        if(set.size()!=0){
+            return set[0];
+        }else{
+            return nums.size()+1;
+        }
+        
+        return 0;
+    }
+};
