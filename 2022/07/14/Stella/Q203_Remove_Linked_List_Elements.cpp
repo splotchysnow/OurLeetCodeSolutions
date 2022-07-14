798f7d994b2ca50860c591c1f661c692234f01fe
yinshinLi <93513827+yinshinLi@users.noreply.github.com>
2022-07-15 01:10:22 +0800

    Update Q203_Remove_Linked_List_Elements.cpp
    listnode definition
    
    Co-Authored-By: Guan Li <46405838+splotchysnow@users.noreply.github.com>

--- 2022/07/14/Stella/Q203_Remove_Linked_List_Elements.cpp
+++ 2022/07/14/Stella/Q203_Remove_Linked_List_Elements.cpp
@@ -1,2 +1,12 @@
 // Given the head of a linked list and an integer val, remove all the nodes of the linked list that has Node.val == val, and return the new head.
 
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
