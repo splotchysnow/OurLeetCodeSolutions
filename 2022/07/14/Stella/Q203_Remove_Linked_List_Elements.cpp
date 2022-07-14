72c98d6baaf26e7e0a2fc03035fd177236e06181
yinshinLi <93513827+yinshinLi@users.noreply.github.com>
2022-07-15 01:10:42 +0800

    Update Q203_Remove_Linked_List_Elements.cpp
    Solution updated
    
    Co-Authored-By: Guan Li <46405838+splotchysnow@users.noreply.github.com>

--- 2022/07/14/Stella/Q203_Remove_Linked_List_Elements.cpp
+++ 2022/07/14/Stella/Q203_Remove_Linked_List_Elements.cpp
@@ -10,3 +10,49 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
+
+class Solution {
+public:
+    ListNode* removeElements(ListNode* head, int val) {
+        ListNode *curr = head;
+        ListNode *sol = head;
+        int h = 0;
+        if(curr==nullptr){
+            return nullptr; 
+        }
+        if(curr->next == nullptr){
+            if(curr->val==val){
+                return nullptr;
+            }else{
+                return head;
+            }
+        }else{
+            if(curr->val==val){
+                sol = curr->next; 
+            }else{
+                 h++;
+            }
+        }
+        while(curr->next!=nullptr){
+            if(curr->next->val == val){
+                if(curr->next->next!=nullptr){
+                    curr->next = curr->next->next;
+                    if(curr->val==val){
+                        sol = curr->next;
+                    }
+                    //curr = curr->next;
+                }else{
+                    curr->next = nullptr;
+                }
+                if(h==0&&curr->val!=val){
+                    sol = curr;
+                }
+            }else{
+                h++;
+                curr = curr->next;
+            }
+        }
+        if(h==0) return nullptr;
+        return sol;
+    }
+};
