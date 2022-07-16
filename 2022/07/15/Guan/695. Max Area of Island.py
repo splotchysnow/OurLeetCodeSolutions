e3f6e72a124b3bf249c649366f38feed4f13a0cb
splotchysnow <46405838+splotchysnow@users.noreply.github.com>
2022-07-16 05:48:30 -0700

    Update 695. Max Area of Island.py
    OMG it Finnally Worked
    
    ALSO DID SET UP THE PYTHON ENV in VSCODE

--- 2022/07/15/Guan/695. Max Area of Island.py
+++ 2022/07/15/Guan/695. Max Area of Island.py
@@ -1,72 +1,77 @@
+# from collections import List
+from typing import List
+from unicodedata import name
+from pip import main
+
 class Solution:
     
     def __init__(self):
         self.value = 0
+        self.h = 0
+        self.v = 0
+        #initialize the visitedMap: (with Falses.)
+        self.visitedMap = []
+        
     
     def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
         
         #get the horizontal and verticle coordinates.
-        h = len(grid[0])
-        v = len(grid)
-        
-        #initialize the visitedMap: (with Falses.)
-        visitedMap = [[0]* (v) for i in range(h)]
-        
-        # print(v,h)
-        # print("VISITED MAP SIZE IS:","v: ", len(visitedMap[0]), "h:", len(visitedMap))
-        # v=8,h=13
-        
-        #We already have the grid for the grids.
+        self.h = len(grid[0])
+        self.v = len(grid)
+        self.value = 0
+        self.visitedMap = [[0]*50 for i in range(50)]
         
         #Create the vairable that keep track of the max area.
         abs_max_area = 0
         local_max = 0
-        #Call the functions:
-        for i in range(h):
-            for j in range(v):
+        
+        # Recursion
+        for i in range(self.v):
+            for j in range(self.h):
                 self.value = 0
-                abs_max_area = max(self.r_check(i,j,grid,visitedMap,local_max),abs_max_area)
-                # if(abs_max_area == 5):
-                #     print(i,j)
-        
-        # self.value = 0
-        # abs_max_area = max(self.r_check(1,1,grid,visitedMap,local_max),abs_max_area)
-        
-        #Return that area.
+                abs_max_area = max(self.r_check(i,j,grid),abs_max_area)
         return abs_max_area
 
     
-    def r_check(self, i:int, j: int,grid: List[List[int]],visited: List[List[int]], value):
-        
-#         if(i == 3 and j == 8):
-#             print(i,j,value)
-        
-        
-        
-        #TODO: UHHH THIS... FIX WORKS FOR NOW.
-        h = len(grid[0])
-        v = len(grid)
-        
-        # print(h,v)
-        if(i>=h or j>=v or i<0 or j<0):
-            pass
+    def r_check(self, i:int, j: int,grid: List[List[int]]) -> int:
         #If the function detects an non-visited island.
-        elif(grid[i][j] == 1 and visited[i][j] == 0):
-            print(i,j,value)
-            #increase value of the area.
-            value+=1
+        if((i<self.v and j<self.h and i>=0 and j>=0) and grid[i][j] == 1 and self.visitedMap[i][j]==0):
             #change visited to true
-            visited[i][j] = 1
+            self.visitedMap[i][j] = 1
+            #increase value of the area.
+            self.value+=1
+            # print("["+str(i)+"]","["+str(j)+"]",self.value)
             #check the closer areas too.
-            self.value = max(value,self.value)
-            self.r_check(i-1,j,grid,visited,value)
-            self.r_check(i+1,j,grid,visited,value)
-            self.r_check(i,j-1,grid,visited,value)
-            self.r_check(i,j+1,grid,visited,value)
-        # print(value,i,j)
+            self.r_check(i-1,j,grid)
+            self.r_check(i,j-1,grid)
+            self.r_check(i+1,j,grid)
+            self.r_check(i,j+1,grid)
+        
         return self.value
+
+def main():
+    a = Solution()
+
+    gri1 = [[0,0,1,0,0,0,0,1,0,0,0,0,0],
+            [0,0,0,0,0,0,0,1,1,1,0,0,0],
+            [0,1,1,0,1,0,0,0,0,0,0,0,0],
+            [0,1,0,0,1,1,0,0,1,0,1,0,0],
+            [0,1,0,0,1,1,0,0,1,1,1,0,0],
+            [0,0,0,0,0,0,0,0,0,0,1,0,0],
+            [0,0,0,0,0,0,0,1,1,1,0,0,0],
+            [0,0,0,0,0,0,0,1,1,0,0,0,0]]
     
-    def print_array(array_: List[List[int]]):
-        for i in range(len(array_)):
-            for j in range(len(array_[0])):
-                print(i,j)
+    gri2 = [[1,0,1,0,0,0,0,1,0,0,0,0,0],
+            [1,0,0,0,0,0,0,1,1,1,0,0,0],
+            [1,1,1,0,1,0,0,0,0,0,0,0,0],
+            [1,1,0,0,1,1,0,0,1,0,1,0,0],
+            [1,1,0,0,1,1,0,0,1,1,1,0,0],
+            [1,0,0,0,0,0,0,0,0,0,1,0,0],
+            [0,0,0,0,0,0,0,1,1,1,0,0,0],
+            [0,0,0,0,0,0,0,1,1,0,0,0,0]]
+
+    print(a.maxAreaOfIsland(gri1))
+    print(a.maxAreaOfIsland(gri2))
+
+if __name__ == "__main__":
+    main()
