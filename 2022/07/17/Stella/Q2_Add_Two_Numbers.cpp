698383b12bb6ede75be119885b186016a83cb01e
yinshinLi <93513827+yinshinLi@users.noreply.github.com>
2022-07-17 23:49:08 -0700

    Create Q2_Add_Two_Numbers.cpp
    question description

---
+++ 2022/07/17/Stella/Q2_Add_Two_Numbers.cpp
@@ -0,0 +1,21 @@
+// You are given two non-empty linked lists representing two non-negative integers. The digits are stored in reverse order, and each of their nodes contains a single digit. Add the two numbers and return the sum as a linked list.
+
+// You may assume the two numbers do not contain any leading zero, except the number 0 itself.
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+
+class Solution {
+public:
+    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+        
+    }
+};
