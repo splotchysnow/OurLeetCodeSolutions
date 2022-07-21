527ce08c196a42bcf704e1762211ba84ac4e8cb6
yinshinLi <93513827+yinshinLi@users.noreply.github.com>
2022-07-21 23:22:32 +0800

    Update Q33_Search_in_Rotated_Sorted_Array.cpp

--- 2022/07/21/Stella/Q33_Search_in_Rotated_Sorted_Array.cpp
+++ 2022/07/21/Stella/Q33_Search_in_Rotated_Sorted_Array.cpp
@@ -5,3 +5,9 @@
 // Given the array nums after the possible rotation and an integer target, return the index of target if it is in nums, or -1 if it is not in nums.
 
 // You must write an algorithm with O(log n) runtime complexity.
+
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+    }
+};
