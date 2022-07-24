0e1b86eb07b2c26f8aab83d7a53e56c7dfa819c1
yinshinLi <93513827+yinshinLi@users.noreply.github.com>
2022-07-24 14:00:57 +0800

    Update Q36_Valid_Sudoku.cpp

--- 2022/07/24/Stella/Q36_Valid_Sudoku.cpp
+++ 2022/07/24/Stella/Q36_Valid_Sudoku.cpp
@@ -8,3 +8,56 @@
 // A Sudoku board (partially filled) could be valid but is not necessarily solvable.
 // Only the filled cells need to be validated according to the mentioned rules.
 
+class Solution {
+public:
+    bool isValidSudoku(vector<vector<char>>& board) {
+        unordered_set<char> row, column, zero, three, six;
+        for(int i=0; i<9; i++){
+            for(int j=0; j<9; j++){
+                if(!(board[i][j]=='.')){
+                    if(row.count(board[i][j])==0){
+                        row.insert(board[i][j]);
+                    }else{
+                        return false;
+                    }
+                    
+                    if(j<=2){
+                        if(zero.count(board[i][j])==0){
+                            zero.insert(board[i][j]);
+                        }else{
+                            return false;
+                        }
+                    }else if(j<=5){
+                        if(three.count(board[i][j])==0){
+                            three.insert(board[i][j]);
+                        }else{
+                            return false;
+                        }
+                    }else{
+                        if(six.count(board[i][j])==0){
+                            six.insert(board[i][j]);
+                        }else{
+                            cout<<"3";
+                            return false;
+                        }
+                    }
+                }
+                if(!(board[j][i]=='.')){
+                    if(column.count(board[j][i])==0){
+                        column.insert(board[j][i]);
+                    }else{
+                        return false;
+                    }
+                }
+            }
+            row.clear();
+            column.clear();
+            if(i==2||i==5){
+                zero.clear();
+                three.clear();
+                six.clear();
+            }
+        }
+        return true;
+    }
+};
